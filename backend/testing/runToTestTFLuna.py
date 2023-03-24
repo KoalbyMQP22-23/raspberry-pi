889bcaa87a24e639ae7e85b2068c95912af17e96
Josh31301 <jmfernandez@wpi.edu>
2023-03-24 16:29:56 -0400

    Start of D-term merge

--- backend/testing/runToTestTFLuna.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from backend.KoalbyHumaniod.Sensors.PiratedCode.BoardDisplay_EKF import initializeCube, ProjectionViewer
-from backend.KoalbyHumaniod.Robot import RealRobot
-
-robot = RealRobot()  # inits real-world robot
-while True:
-    data = robot.get_tf_luna_data()  # gets TF Luna data
-    print(data)
